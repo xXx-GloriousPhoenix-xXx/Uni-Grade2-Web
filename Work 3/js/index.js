e8448a93a13c2fd96083aa15a85c1c14c1467a34
RomanPR <prikhodjko.roman@gmail.com>
2024-11-16 04:20:02 +0200

    v1.0
    PC, Tablet, Mobile

--- Work 3/js/index.js
+++ Work 3/js/index.js
@@ -10,11 +10,15 @@ const name = {
     cs: "cs.html",
     html: "html.html"
 };
+const external = {
+    github: "https://github.com/xXx-GloriousPhoenix-xXx"
+};
 
 for (const id in name) {
-    if (name.hasOwnProperty(id)) {
-        document.getElementById(id).addEventListener("click", () => {
-            document.location.href = directory + name[id];
-        });
-    }
+    document.getElementById(id).addEventListener("click", () => {
+        document.location.href = directory + name[id];
+    });
 }
+document.getElementById("github").addEventListener("click", () => {
+    document.location.href = external["github"];
+});
