1fa1993b7c25bdf5a9f1c7de269d16cfca708557
RomanPR <prikhodjko.roman@gmail.com>
2024-11-17 06:31:37 +0200

    Main done

--- Main/js/index.js
+++ Main/js/index.js
@@ -1,5 +1,31 @@
 function positionButtons() {
-    const radius = container.offsetHeight / 2;
+    const width = document.documentElement.offsetWidth;
+    let divider = 1;
+    if (width >= 1366) 
+    {
+        divider = 2;
+    }
+    else 
+    {
+        if (769 <= width && width <= 1365) {
+            divider = 2.8;
+        }
+        else 
+        {
+            if (449 <= width && width <= 768) 
+            {
+                divider = 3;
+            }
+            else (width <= 448) 
+            {
+                divider = 1.9;
+            }
+        }
+    }
+    console.log(divider);
+    console.log(container.offsetWidth);
+
+    const radius = Math.min(container.offsetHeight, container.offsetWidth) / divider;
 
     buttons.forEach(({ element, buttonRotation }, index) => {
         const angle = (index / buttons.length) * (2 * Math.PI);
@@ -10,6 +36,39 @@ function positionButtons() {
         element.style.top = `${y}px`;
         element.style.transform = `translate(-50%, -50%) rotate(${buttonRotation}deg)`;
     });
+}
+function resizeButtons () {
+    buttons.forEach(({element}) => {
+        let size = getSize();
+        element.style.width = `${size}dvh`;
+        element.style.height = `${size}dvh`;
+    });
+}
+function getSize() {
+    const width = document.documentElement.offsetWidth;
+    let size = 0;
+    if (width >= 1366) 
+    {
+        size = getRandomInt(12, 16);
+    }
+    else 
+    {
+        if (769 <= width && width <= 1365) {
+            size = getRandomInt(10, 14);
+        }
+        else 
+        {
+            if (449 <= width && width <= 768) 
+            {
+                size = getRandomInt(8, 12);
+            }
+            else (width <= 448) 
+            {
+                size = getRandomInt(4, 8);
+            }
+        }
+    }
+    return size;
 }
 async function countWorks() {
     const repoOwner = 'xXx-GloriousPhoenix-xXx';
@@ -26,7 +85,6 @@ function getRandomInt(min, max) {
     return Math.random() * (intMax - intMin) + intMin;
 }
 function rotate() {
-    // angleDifference += 1 / 32; 
     const angleDifference = 1 / 32;
     systemRotationAngle = (systemRotationAngle + angleDifference) % 360;
     container.style.transform = `rotate(${systemRotationAngle}deg)`;
@@ -43,7 +101,7 @@ async function start() {
     console.log(worksDone);
 
     const buttonCount = worksDone;
-    const radius = container.offsetHeight / 2;
+    const radius = Math.min(container.offsetHeight, container.offsetWidth) / 2;
 
     for (let i = 0; i < buttonCount; i++) {
         const angle = (i / buttonCount) * (2 * Math.PI);
@@ -54,9 +112,6 @@ async function start() {
         button.style.left = `${x}px`;
         button.style.top = `${y}px`;
 
-        const size = getRandomInt(10, 16);
-        button.style.width = `${size}dvh`;
-        button.style.height = `${size}dvh`;
         button.style.backgroundImage = `url('Main/data/planet_${i + 1}.png')`;
         button.addEventListener("click", () => {
             document.location.href = `https://xxx-gloriousphoenix-xxx.github.io/Uni-Grade2-Web/Work%20${i + 1}`;
@@ -70,12 +125,13 @@ async function start() {
         container.appendChild(button);
     }
 
+    window.addEventListener("resize", positionButtons);
+    window.addEventListener("resize", resizeButtons);
+
+    resizeButtons();  
     positionButtons();
     rotate();
 }
-
-// Обновление позиций кнопок при изменении размера окна
-window.addEventListener("resize", positionButtons);
 
 const buttons = [];
 let systemRotationAngle = 0;
