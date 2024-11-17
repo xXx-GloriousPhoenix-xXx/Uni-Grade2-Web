b1abe68a836b640d5e8b1d760e05766c33856a01
RomanPR <prikhodjko.roman@gmail.com>
2024-11-17 05:27:14 +0200

    Main v1.0

--- Main/js/index copy.js
+++ Main/js/index copy.js
@@ -1,16 +1,3 @@
-function positionButtons() {
-    const radius = container.offsetHeight / 2;
-
-    buttons.forEach(({ element, buttonRotation }, index) => {
-        const angle = (index / buttons.length) * (2 * Math.PI);
-        const x = radius * Math.cos(angle) + container.offsetWidth / 2;
-        const y = radius * Math.sin(angle) + container.offsetHeight / 2;
-
-        element.style.left = `${x}px`;
-        element.style.top = `${y}px`;
-        element.style.transform = `translate(-50%, -50%) rotate(${buttonRotation}deg)`;
-    });
-}
 async function countWorks() {
     const repoOwner = 'xXx-GloriousPhoenix-xXx';
     const repoName = 'Uni-Grade2-Web';
@@ -27,7 +14,7 @@ function getRandomInt(min, max) {
 }
 function rotate() {
     // angleDifference += 1 / 32; 
-    const angleDifference = 1 / 32;
+    const angleDifference = 1;
     systemRotationAngle = (systemRotationAngle + angleDifference) % 360;
     container.style.transform = `rotate(${systemRotationAngle}deg)`;
 
@@ -38,47 +25,43 @@ function rotate() {
 
     requestAnimationFrame(rotate);
 }
-async function start() {
-    const worksDone = await countWorks();
-    console.log(worksDone);
-
-    const buttonCount = worksDone;
-    const radius = container.offsetHeight / 2;
-
-    for (let i = 0; i < buttonCount; i++) {
-        const angle = (i / buttonCount) * (2 * Math.PI);
-        const x = radius * Math.cos(angle) + container.offsetWidth / 2;
-        const y = radius * Math.sin(angle) + container.offsetHeight / 2;
-
-        const button = document.createElement("button");
-        button.style.left = `${x}px`;
-        button.style.top = `${y}px`;
-
-        const size = getRandomInt(10, 16);
-        button.style.width = `${size}dvh`;
-        button.style.height = `${size}dvh`;
-        button.style.backgroundImage = `url('Main/data/planet_${i + 1}.png')`;
-        button.addEventListener("click", () => {
-            document.location.href = `https://xxx-gloriousphoenix-xxx.github.io/Uni-Grade2-Web/Work%20${i + 1}`;
-        });
-
-        button.textContent = i + 1;
-
-        const buttonRotation = getRandomInt(0, 360);
-
-        buttons.push({ element: button, buttonRotation: buttonRotation });
-        container.appendChild(button);
-    }
-
-    positionButtons();
-    rotate();
-}
-
-// Обновление позиций кнопок при изменении размера окна
-window.addEventListener("resize", positionButtons);
 
 const buttons = [];
 let systemRotationAngle = 0;
 const container = document.getElementById("container");
+
+async function start() {
+
+    const worksDone = await countWorks();
+    console.log(worksDone);
+    
+    const buttonCount = worksDone;
+    const radius = container.offsetHeight / 2;
+    
+    for (let i = 0; i < buttonCount; i++) {
+        const angle = (i / buttonCount) * (2 * Math.PI);
+        const x = radius * Math.cos(angle) + container.offsetWidth / 2;
+        const y = radius * Math.sin(angle) + container.offsetHeight / 2;
+    
+        const button = document.createElement("button");
+        button.style.left = `${x}px`;
+        button.style.top = `${y}px`;
+    
+        const size = getRandomInt(10, 16);
+        button.style.width = `${size}dvh`;
+        button.style.height = `${size}dvh`;
+        button.style.backgroundImage = `url('Main/data/planet_${i + 1}.png')`;
+        button.addEventListener("click", () => document.location.href = `https://xxx-gloriousphoenix-xxx.github.io/Uni-Grade2-Web/Work%20${i + 1}`);
+    
+        button.textContent = i + 1;
+    
+        const buttonRotation = getRandomInt(0, 360); 
+    
+        buttons.push({ element: button, radius: radius, buttonRotation: buttonRotation });
+        container.appendChild(button);
+    }
+
+    rotate();
+}
 
 start();
