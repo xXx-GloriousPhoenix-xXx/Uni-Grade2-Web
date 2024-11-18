96dacd37ae4c35e848d5858fdaf97356324ca7fb
RomanPR <prikhodjko.roman@gmail.com>
2024-11-18 17:19:33 +0200

    UI update

---
+++ Work 4/js/index.js
@@ -0,0 +1,19 @@
+function DisplayMenu() 
+{
+    if (menu.style.transform === "rotate(0deg)") 
+    {
+        menu.style.transform = "rotate(90deg)";
+        tooltip.style.display = "flex";
+    }
+    else 
+    {
+        menu.style.transform = "rotate(0deg)";
+        tooltip.style.display = "none";
+    }
+}
+
+const menu = document.getElementById("menu");
+menu.addEventListener("click", DisplayMenu);
+const tooltip = document.getElementById("tooltip");
+
+
